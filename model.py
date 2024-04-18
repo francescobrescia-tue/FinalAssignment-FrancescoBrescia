fc62dcb603a1871308ad0a85166a0e29b40ab80e
francescobrescia-tue <159447134+francescobrescia-tue@users.noreply.github.com>
2024-04-18 14:42:47 +0200

    Add Deeper Unet

--- model.py
+++ model.py
@@ -1,166 +1,62 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms.functional as TF
 
-class ConvBlock(nn.Module):
+class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(ConvBlock, self).__init__()
+        super(DoubleConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
         )
 
     def forward(self, x):
         return self.conv(x)
 
-class UpConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(UpConv, self).__init__()
-        self.up = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        return self.up(x)
-
-class AttentionBlock(nn.Module):
-    def __init__(self, F_g, F_l, n_coefficients):
-        super(AttentionBlock, self).__init__()
-        self.W_gate = nn.Sequential(
-            nn.Conv2d(F_g, n_coefficients, kernel_size=1),
-            nn.BatchNorm2d(n_coefficients)
-        )
-        self.W_x = nn.Sequential(
-            nn.Conv2d(F_l, n_coefficients, kernel_size=1),
-            nn.BatchNorm2d(n_coefficients)
-        )
-        self.psi = nn.Sequential(
-            nn.Conv2d(n_coefficients, 1, kernel_size=1),
-            nn.BatchNorm2d(1),
-            nn.Sigmoid()
-        )
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, gate, skip_connection):
-        g1 = self.W_gate(gate)
-        x1 = self.W_x(skip_connection)
-        psi = self.relu(g1 + x1)
-        psi = self.psi(psi)
-        out = skip_connection * psi
-        return out
-
 class Model(nn.Module):
-    def __init__(self, img_ch=3, output_ch=19):
+    def __init__(self, in_channels=3, out_channels=19, features=[16, 32, 64, 128, 256, 512, 1024, 2048]):
         super(Model, self).__init__()
-        self.MaxPool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # Doubling pattern in encoder
-        self.Conv1 = ConvBlock(img_ch, 16)
-        self.Conv2 = ConvBlock(16, 32)
-        self.Conv3 = ConvBlock(32, 64)
-        self.Conv4 = ConvBlock(64, 128)
-        self.Conv5 = ConvBlock(128, 256)
-        self.Conv6 = ConvBlock(256, 512)
-        self.Conv7 = ConvBlock(512, 1024)
-        self.Conv8 = ConvBlock(1024, 2048)
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        # Halving pattern in decoder
-        self.Up8 = UpConv(2048, 1024)
-        self.Att8 = AttentionBlock(F_g=1024, F_l=1024, n_coefficients=512)
-        self.UpConv8 = ConvBlock(2048, 1024)
+        # Down part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
 
-        self.Up7 = UpConv(1024, 512)
-        self.Att7 = AttentionBlock(F_g=512, F_l=512, n_coefficients=256)
-        self.UpConv7 = ConvBlock(1024, 512)
+        # Up part of UNET
+        for feature in reversed(features):
+            self.ups.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2, padding=0))
+            self.ups.append(DoubleConv(feature*2, feature))
 
-        self.Up6 = UpConv(512, 256)
-        self.Att6 = AttentionBlock(F_g=256, F_l=256, n_coefficients=128)
-        self.UpConv6 = ConvBlock(512, 256)
-
-        self.Up5 = UpConv(256, 128)
-        self.Att5 = AttentionBlock(F_g=128, F_l=128, n_coefficients=64)
-        self.UpConv5 = ConvBlock(256, 128)
-
-        self.Up4 = UpConv(128, 64)
-        self.Att4 = AttentionBlock(F_g=64, F_l=64, n_coefficients=32)
-        self.UpConv4 = ConvBlock(128, 64)
-
-        self.Up3 = UpConv(64, 32)
-        self.Att3 = AttentionBlock(F_g=32, F_l=32, n_coefficients=16)
-        self.UpConv3 = ConvBlock(64, 32)
-
-        self.Up2 = UpConv(32, 16)
-        self.Att2 = AttentionBlock(F_g=16, F_l=16, n_coefficients=8)
-        self.UpConv2 = ConvBlock(32, 16)
-
-        self.FinalConv = nn.Conv2d(16, output_ch, kernel_size=1)
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
     def forward(self, x):
-        e1 = self.Conv1(x)
-        
-        e2 = self.MaxPool(e1)
-        e2 = self.Conv2(e2)
-        
-        e3 = self.MaxPool(e2)
-        e3 = self.Conv3(e3)
-        
-        e4 = self.MaxPool(e3)
-        e4 = self.Conv4(e4)
-        
-        e5 = self.MaxPool(e4)
-        e5 = self.Conv5(e5)
-        
-        e6 = self.MaxPool(e5)
-        e6 = self.Conv6(e6)
-        
-        e7 = self.MaxPool(e6)
-        e7 = self.Conv7(e7)
-        
-        e8 = self.MaxPool(e7)
-        e8 = self.Conv8(e8)
-        
-        # Start decoder
-        d8 = self.Up8(e8)
-        s7 = self.Att8(gate=d8, skip_connection=e7)
-        d8 = torch.cat((s7, d8), dim=1)
-        d8 = self.UpConv8(d8)
-        
-        d7 = self.Up7(d8)
-        s6 = self.Att7(gate=d7, skip_connection=e6)
-        d7 = torch.cat((s6, d7), dim=1)
-        d7 = self.UpConv7(d7)
-        
-        d6 = self.Up6(d7)
-        s5 = self.Att6(gate=d6, skip_connection=e5)
-        d6 = torch.cat((s5, d6), dim=1)
-        d6 = self.UpConv6(d6)
-        
-        d5 = self.Up5(d6)
-        s4 = self.Att5(gate=d5, skip_connection=e4)
-        d5 = torch.cat((s4, d5), dim=1)
-        d5 = self.UpConv5(d5)
-        
-        d4 = self.Up4(d5)
-        s3 = self.Att4(gate=d4, skip_connection=e3)
-        d4 = torch.cat((s3, d4), dim=1)
-        d4 = self.UpConv4(d4)
-        
-        d3 = self.Up3(d4)
-        s2 = self.Att3(gate=d3, skip_connection=e2)
-        d3 = torch.cat((s2, d3), dim=1)
-        d3 = self.UpConv3(d3)
-        
-        d2 = self.Up2(d3)
-        s1 = self.Att2(gate=d2, skip_connection=e1)
-        d2 = torch.cat((s1, d2), dim=1)
-        d2 = self.UpConv2(d2)
-        
-        out = self.FinalConv(d2)
+        skip_connections = []
 
-        return out
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx+1](concat_skip)
+
+        return self.final_conv(x)
